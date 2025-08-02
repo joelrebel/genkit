d1dacb877829a6d6fe1be24ce0a87dabf3ce6b99
jrebello <jrebello@cloudflare.com>
2025-08-02 15:49:37 +0200

    Fixes and support Qwen3 and newer models

--- go/plugins/workersai/models.go
+++ go/plugins/workersai/models.go
@@ -6,6 +6,7 @@ const (
 	mistralSmall3124BInstruct     = "@cf/mistralai/mistral-small-3.1-24b-instruct"
 	metaLlama3370bInstructFp8Fast = "@cf/meta/llama-3.3-70b-instruct-fp8-fast"
 	metaLlama4scout17b16einstruct = "@cf/meta/llama-4-scout-17b-16e-instruct"
+	qwenQwen330ba3bfp8            = "@cf/qwen/qwen3-30b-a3b-fp8"
 )
 
 var (
@@ -43,6 +44,16 @@ var (
 				Multiturn:  true,
 				Tools:      true,
 				ToolChoice: false,
+				SystemRole: true,
+				Media:      true,
+			},
+		},
+		qwenQwen330ba3bfp8: {
+			Label: qwenQwen330ba3bfp8,
+			Supports: &ai.ModelSupports{
+				Multiturn:  true,
+				Tools:      true,
+				ToolChoice: true,
 				SystemRole: true,
 				Media:      true,
 			},
